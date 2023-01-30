ec7ebafcfc5fc5fc6e3ccbcc1bd99913a04e66a0
Laiba <laibasajid4444@gmail.com>
2023-01-31 00:02:08 +0500

    i have added three files of jocke project

---
+++ script.js
@@ -0,0 +1,52 @@
+
+const jokes = document.querySelector('#joke');
+const jokeBtn = document.querySelector('#jokeBtn');
+
+//**************************************************** though promises*********************************************
+
+// const generateJokes = ( ) =>{
+
+// const setHeader = {
+//  headers: {
+// Accept : "application/json"
+//     }
+// }
+
+
+// fetch('https://icanhazdadjoke.com' , setHeader)
+// .then ((res) => res.json() )
+// .then( (data) => {
+//     jokes.innerHTML = data.joke;
+// }).catch( (error) => {
+// console.log(error);
+// })
+
+// }
+
+
+
+//**************************************************** though Assyncrones await *********************************************
+
+
+
+
+const generateJokes =  async ( ) =>{
+
+    try{
+        const setHeader = {
+            headers: {
+           Accept : "application/json"
+               }
+           }
+           const res = await fetch('https://icanhazdadjoke.com' , setHeader);
+           const data = await res.json();
+           jokes.innerHTML = data.joke;
+    }catch(err){
+        console.log(`the error is ${err}`);
+    }
+
+
+
+}
+ jokeBtn.addEventListener('click', generateJokes);
+generateJokes();
